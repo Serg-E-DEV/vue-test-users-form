51fe4d74631c951bf18db66a7826d3b6300dbef4
Sergey E <sibharakter@gmail.com>
2025-08-03 11:08:14 +0700

    feat: loading and saving accounts to localStorage

--- src/stores/accounts.store.ts
+++ src/stores/accounts.store.ts
@@ -1,21 +1,41 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { nanoid } from 'nanoid';
 import { AccountInterface } from '@/interfaces/account.interface';
+
+const ACCOUNTS_STORAGE_KEY = 'accounts-storage';
 
 export const useAccountsStore = defineStore('AccountsStore', () => {
   const accounts = ref<AccountInterface[]>([]);
 
+  function loadAccounts() {
+    const accountsData = localStorage.getItem(ACCOUNTS_STORAGE_KEY);
+    if (!accountsData) {
+      return;
+    }
+    try {
+      accounts.value = JSON.parse(accountsData);
+    } catch (e) {
+      accounts.value = [];
+      console.error(e);
+    }
+  }
+
+  function saveAccounts() {
+    localStorage.setItem(ACCOUNTS_STORAGE_KEY, JSON.stringify(accounts.value));
+  }
+
   function createAccount() {
     const newAccount: AccountInterface = {
       id: nanoid(),
-      recordLabel: '',
+      recordLabel: 'XXX; YYYY; II; MMM',
       recordType: 'local',
       login: '',
       password: '',
     };
 
     accounts.value.push(newAccount);
+    saveAccounts();
   }
   function removeAccount(id: string) {
     if (!id) {
@@ -29,6 +49,10 @@ export const useAccountsStore = defineStore('AccountsStore', () => {
       account.id === updatedAccount.id ? { ...updatedAccount } : account
     );
   }
+
+  watch(accounts, saveAccounts, { deep: true });
+
+  loadAccounts();
 
   return {
     accounts,
