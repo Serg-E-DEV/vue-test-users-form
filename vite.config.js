6bdd1434f1bbc0097203d2d34cd9a4de102fc934
Sergey E <sibharakter@gmail.com>
2025-08-03 18:06:31 +0700

    build: configure base path for production mode

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: mode === 'production' ? '/en/10-bookmarks/' : '/',
+  base: mode === 'production' ? '/test-tasks/3-vue-users-form/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
